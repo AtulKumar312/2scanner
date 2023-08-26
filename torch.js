af84078eab2c8a7fe407003e69c1c3a80ad5578d
AtulKumar312 <141059121+AtulKumar312@users.noreply.github.com>
2023-08-26 21:08:10 +0530

    Add files via upload

---
+++ torch.js
@@ -0,0 +1,14 @@
+// torch.js
+const toggleTorchButton = document.getElementById("toggleTorch");
+toggleTorchButton.addEventListener("click", toggleTorch);
+
+let torchEnabled = false;
+
+function toggleTorch() {
+    if (torchEnabled) {
+        qrCodeScanner.turnOffFlash(); // Turn off the torch
+    } else {
+        qrCodeScanner.turnOnFlash(); // Turn on the torch
+    }
+    torchEnabled = !torchEnabled; // Toggle torch status
+}
